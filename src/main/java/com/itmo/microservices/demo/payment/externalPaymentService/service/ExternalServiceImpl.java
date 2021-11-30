19ec88a0b09994d2dc44a4538846d6b969305621
cognitio <vladislav.sivirin@tune-it.ru>
2021-11-30 23:55:46 +0300

    Implement super basic fault-tolerance

--- src/main/java/com/itmo/microservices/demo/payment/externalPaymentService/service/ExternalServiceImpl.java
+++ src/main/java/com/itmo/microservices/demo/payment/externalPaymentService/service/ExternalServiceImpl.java
@@ -3,26 +3,43 @@ package com.itmo.microservices.demo.payment.externalPaymentService.service;
 import com.itmo.microservices.demo.payment.PaymentServiceConstants;
 import com.itmo.microservices.demo.payment.externalPaymentService.client.ExternalServiceClient;
 import com.itmo.microservices.demo.payment.externalPaymentService.client.model.ExternalServiceRequest;
+import com.itmo.microservices.demo.payment.externalPaymentService.client.model.ExternalServiceResponse;
 import com.itmo.microservices.demo.payment.externalPaymentService.client.model.ExternalServiceResponseStatus;
 import com.itmo.microservices.demo.payment.externalPaymentService.service.exception.ExternalServiceException;
 import com.itmo.microservices.demo.payment.externalPaymentService.service.model.ExternalServicePayment;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ExternalServiceImpl implements ExternalService {
 
     private final ExternalServiceClient client;
 
+    private final int maxAttempts = 3;
+
     @Override
     public ExternalServicePayment executePayment(String clientSecret) throws ExternalServiceException {
-        var clientResponse = client.executePayment(ExternalServiceRequest.create(clientSecret));
+        ExternalServiceResponse clientResponse;
 
-        if (clientResponse == null) {
-            throw new ExternalServiceException(String.format(
-                    "%s Cannot execute payment transaction with client secret: '%s'. Reason: client error",
-                    PaymentServiceConstants.EXTERNAL_SERVICE_MARKER, clientSecret));
+        for (int attempt = 1; ; ++attempt) {
+            clientResponse = client.executePayment(ExternalServiceRequest.create(clientSecret));
+
+            if (clientResponse == null) {
+                if (attempt == maxAttempts) {
+                    throw new ExternalServiceException(String.format(
+                            "%s Cannot execute payment transaction with client secret: '%s'. Reason: client error. " +
+                                    "It was last attempt: %s",
+                            PaymentServiceConstants.EXTERNAL_SERVICE_MARKER, clientSecret, attempt));
+                } else {
+                    log.info("{} Transaction with client secret: {} client error. Attempt: {} of {}. Retrying ...",
+                            PaymentServiceConstants.EXTERNAL_SERVICE_MARKER, clientSecret, attempt, maxAttempts);
+                }
+            } else {
+                break;
+            }
         }
 
         if (clientResponse.getStatus() == ExternalServiceResponseStatus.FAILURE) {
