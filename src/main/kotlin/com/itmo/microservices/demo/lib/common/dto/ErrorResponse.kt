f75dd3b9c79197ae5a260258d03151651b169487
cognitio <vladislav.sivirin@tune-it.ru>
2021-11-30 23:19:02 +0300

    Add common ErrorResponse for controller error handling

---
+++ src/main/kotlin/com/itmo/microservices/demo/lib/common/dto/ErrorResponse.kt
@@ -0,0 +1,5 @@
+package com.itmo.microservices.demo.lib.common.dto
+
+data class ErrorResponse(
+        var error: String
+)
--- src/main/kotlin/com/itmo/microservices/demo/order/api/controller/controllerAdvice/OrderControllerAdvice.kt
+++ src/main/kotlin/com/itmo/microservices/demo/order/api/controller/controllerAdvice/OrderControllerAdvice.kt
@@ -1,5 +1,6 @@
 package com.itmo.microservices.demo.order.api.controller.controllerAdvice
 
+import com.itmo.microservices.demo.lib.common.dto.ErrorResponse
 import com.itmo.microservices.demo.payment.api.exception.PaymentServiceException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,9 +12,8 @@ import org.springframework.web.context.request.WebRequest
 class OrderControllerAdvice {
 
     @ExceptionHandler(value = [(PaymentServiceException::class)])
-    fun handlePaymentServiceException(e: PaymentServiceException, request: WebRequest): ResponseEntity<String> {
-        val errorString = e.message;
+    fun handlePaymentServiceException(e: PaymentServiceException, request: WebRequest): ResponseEntity<ErrorResponse> {
 
-        return ResponseEntity(errorString, HttpStatus.BAD_REQUEST);
+        return ResponseEntity(ErrorResponse(e.message ?: "Unknown error"), HttpStatus.BAD_REQUEST);
     }
 }
