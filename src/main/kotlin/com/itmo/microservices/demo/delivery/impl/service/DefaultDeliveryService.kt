2c8271e0758e2c9a2af1b790bf3660297b22581d
Lebedev Ilya <ilya.lebedev@jetbrains.com>
2022-03-02 18:29:20 +0300

    fix counter OrderCreated

--- src/main/kotlin/com/itmo/microservices/demo/delivery/impl/service/DefaultDeliveryService.kt
+++ src/main/kotlin/com/itmo/microservices/demo/delivery/impl/service/DefaultDeliveryService.kt
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.util.*
-import org.springframework.beans.factory.annotation.Value
+
 
 @Service
 class DefaultDeliveryService : DeliveryService {
