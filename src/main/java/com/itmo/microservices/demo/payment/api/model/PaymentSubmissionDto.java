ff48001c52263919b8da528dbc5facc1d0aa2be1
dstukalov <dmitriy.stukalov@emergn.com>
2022-04-12 18:21:10 +0300

    Stukalov fix field name

--- src/main/java/com/itmo/microservices/demo/payment/api/model/PaymentSubmissionDto.java
+++ src/main/java/com/itmo/microservices/demo/payment/api/model/PaymentSubmissionDto.java
@@ -1,17 +1,29 @@
 package com.itmo.microservices.demo.payment.api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class PaymentSubmissionDto {
     private Long timestamp;
-    private UUID transactionID;
+    private UUID transactionId;
+
+    public PaymentSubmissionDto(Long timestamp, UUID transactionId) {
+        this.timestamp = timestamp;
+        this.transactionId = transactionId;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public UUID getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(UUID transactionId) {
+        this.transactionId = transactionId;
+    }
 }
